7c1d81be393d38c4d134c350bbb3360e2046a7e8
Evgeny Nacu <evgeny.nacu@gmail.com>
2020-06-19 17:52:27 +0300

    fix(*): added exports using index in rxjs-react

---
+++ packages/rxjs-react/src/index.ts
@@ -0,0 +1,7 @@
+import { Rx } from "./rx"
+import { R } from "./rx-html"
+import { useRx } from "./use-rx"
+import { useRxChange } from "./use-rx-change"
+import { useSubscription } from "./use-subscription"
+
+export { Rx, R, useRx, useRxChange, useSubscription }
