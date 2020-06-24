f761333279bb712d6d6c8ddbb95ad100364a29ab
Evgeny Nacu <evgeny.nacu@gmail.com>
2020-06-24 10:02:15 +0300

    fix(*): export props in index

--- packages/rxjs-react/src/index.ts
+++ packages/rxjs-react/src/index.ts
@@ -1,9 +1,9 @@
-import { RxWrapper } from "./rx-wrapper"
+import { RxWrapper, RxWrapperProps } from "./rx-wrapper"
 import { R } from "./rx-html"
 import { useRx } from "./use-rx"
 import { useRxChange } from "./use-rx-change"
 import { useSubscription } from "./use-subscription"
 import { lift } from "./lift"
-import { RxIf } from "./rx-if"
+import { RxIf, RxIfProps } from "./rx-if"
 
-export { RxWrapper, RxIf, R, useRx, useRxChange, useSubscription, lift }
+export { RxWrapper, RxWrapperProps, RxIf, RxIfProps, R, useRx, useRxChange, useSubscription, lift }
