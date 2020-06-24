2d1aefa11044858457e5820d89c785553309ae70
Evgeny Nacu <evgeny.nacu@gmail.com>
2020-06-24 06:41:09 +0300

    fix(*): export improved

--- packages/rxjs-atom-promise/src/index.ts
+++ packages/rxjs-atom-promise/src/index.ts
@@ -1,6 +1,20 @@
 import { PromiseState, createPromiseStateIdle, createPromiseStatePending, createPromiseStateRejected, createPromiseStateFulfilled } from "./promise-state"
+import { CacheImpl, Cache } from "./cache"
+import { KeyCache, KeyCacheImpl } from "./key-cache"
+import { Cacheable } from "./cacheable"
+import { Rx } from "./rx"
+import { save } from "./save"
+import { useRxWithStatus } from "./use-rx-with-status"
 
 export {
+	Cache,
+	CacheImpl,
+	KeyCache,
+	KeyCacheImpl,
+	Cacheable,
+	Rx,
+	save,
+	useRxWithStatus,
 	PromiseState,
 	createPromiseStateIdle,
 	createPromiseStatePending,
