4034db578f4e75ab71d4cf5fc919ceb77f5232c7
Evgeny Nacu <evgeny.nacu@gmail.com>
2020-11-21 22:49:05 +0300

    fix(*): Cache set and modify

--- packages/rxjs-atom-promise/src/cache.ts
+++ packages/rxjs-atom-promise/src/cache.ts
@@ -1,10 +1,12 @@
 import { Atom } from "@rixio/rxjs-atom/build"
-import { getFinalValue, PromiseState } from "./promise-state"
+import { getFinalValue, PromiseState, createPromiseStateFulfilled } from "./promise-state"
 import { save } from "./save"
 
 export interface Cache<T> {
 	load(): Promise<T>
 	get(force?: boolean): Promise<T>
+	set(value: T): void
+	modify(fn: (value: T) => T): void
 	atom: Atom<PromiseState<T>>
 }
 
@@ -22,6 +24,20 @@ export class CacheImpl<T> implements Cache<T> {
 	get(force: boolean = false): Promise<T> {
 		const state$ = this.getAtomAndLoad(force)
 		return getFinalValue(state$)
+	}
+
+	modify(fn: (value: T) => T): void {
+		this.atom.modify(ps => {
+			if (ps.status === "fulfilled") {
+				return createPromiseStateFulfilled(fn(ps.value))
+			} else {
+				return ps
+			}
+		})
+	}
+
+	set(value: T): void {
+		this.atom.set(createPromiseStateFulfilled(value))
 	}
 
 	private getAtomAndLoad(force: boolean = false): Atom<PromiseState<T>> {
