d994f6f33ace8e6aa7bec61d813741354505ca18
Evgeny Nacu <evgeny.nacu@gmail.com>
2020-11-23 17:55:00 +0300

    fix(*): useAtom, simplier version of useRx

--- packages/rxjs-cache/src/index.ts
+++ packages/rxjs-cache/src/index.ts
@@ -40,6 +40,7 @@ export function toWrapped<T>(cache: CacheState<T>): Wrapped<T> {
 	switch (cache.status) {
 		case "fulfilled":
 			return createFulfilledWrapped(cache.value)
+		case "idle":
 		case "pending":
 			return pendingWrapped
 		case "rejected":
