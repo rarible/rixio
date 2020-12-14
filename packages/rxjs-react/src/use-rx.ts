4dac9d5fdd97a83376f1352656e825fcc1d1bfcd
Evgeny Nacu <evgeny.nacu@gmail.com>
2020-12-14 08:42:42 +0300

    fix(*): fix useRx, only one subscribe is needed

--- packages/rxjs-react/src/use-rx.ts
+++ packages/rxjs-react/src/use-rx.ts
@@ -1,7 +1,7 @@
-import { useState, useMemo, useRef } from "react"
+import { useState, useMemo, useRef, useEffect } from "react";
 import { Observable } from "rxjs"
 import { first } from "rxjs/operators"
-import { Wrapped, wrap, WrappedObservable, toPlainOrThrow } from "@rixio/rxjs-wrapped"
+import { Wrapped, wrap, WrappedObservable, toPlainOrThrow, pendingWrapped } from "@rixio/rxjs-wrapped";
 import { ReadOnlyAtom } from "@rixio/rxjs-atom"
 import { useSubscription } from "./use-subscription"
 
@@ -37,26 +37,28 @@ export function getImmediateOrThrow<T>(observable: Observable<T>): T {
 }
 
 export function useRx<T>(observable: WrappedObservable<T>, deps: any[] = []): Wrapped<T> {
+	const [, setCount] = useState<number>(0)
 	const wrapped = useMemo(() => wrap(observable), [observable])
-	const [state, setState] = useState<Wrapped<T>>(() => getImmediateOrThrow(wrapped))
-	const ref = useRef<Wrapped<T>>(state)
-	useSubscription(
-		wrapped,
-		value => {
-			const current = ref.current
-			if (current.status === "fulfilled") {
-				if (value.status !== "fulfilled" || value.value !== current.value) {
-					ref.current = value
-					setState(value)
-				}
-			} else {
-				ref.current = value
-				setState(value)
+	const value = useRef<Wrapped<T>>(pendingWrapped)
+	const initial = useRef(true)
+	const sub = useMemo(() => wrapped.subscribe(next => {
+		const current = value.current
+		value.current = next
+		if (!initial.current) {
+			if (
+				current.status !== next.status ||
+				(current.status === "fulfilled" && next.status === "fulfilled" && current.value !== next.value)
+			) {
+				setCount(c => c + 1)
 			}
-		},
-		deps
-	)
-	return state
+		}
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}), [wrapped, ...deps])
+	useEffect(() => {
+		return () => sub.unsubscribe()
+	}, [sub])
+	initial.current = false
+	return value.current
 }
 
 export function useRxOrThrow<T>(observable: WrappedObservable<T>): T {
